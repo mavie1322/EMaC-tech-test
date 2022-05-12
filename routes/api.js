40822ac5080f32e71727d4e3ceabe797dd327368
BG <mavie1322@yahoo.fr>
2022-05-12 11:49:17 +0100

    add endpoint /api/recipes and handle error if the endpoint is incorrect

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,23 @@
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
+const fs = require("fs");
+const dataPath = "./data/data.json";
 
-apiRouter.get('/', (_, res) => {
-  res.json({ message: 'ok' });
+apiRouter.get("/", (_, res) => {
+  res.json({ message: "ok" });
+});
+
+apiRouter.get("/recipes", (req, res) => {
+  console.log(req);
+  fs.readFile(dataPath, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send({ recipes: JSON.parse(data) });
+  });
+});
+
+apiRouter.all("*", (_, res) => {
+  res.status(404).send({ message: "Invalid url" });
 });
 
 module.exports = apiRouter;
